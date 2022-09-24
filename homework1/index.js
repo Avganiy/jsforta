c045d8d6ffb7334a53427294e8cd1a006290e268
avganiy <ekramarets@gmail.com>
2022-09-24 12:07:49 +0300

    added task 1 and 2

--- homework1/index.js
+++ homework1/index.js
@@ -1,3 +1,6 @@
 // task 1
-
+let userName = prompt('Please enter First Name below:', 'John');
+alert(`Welcome ${userName}!`);
 // task 2
+let lastName = prompt('Please enter Last Name below:', 'Doe');
+alert(`Welcome ${lastName}!`);
---
+++ homework1/surname-alert.js
@@ -0,0 +1,2 @@
+// task 2
+let lastName = prompt('Please enter Last Name below:', 'Doe');
