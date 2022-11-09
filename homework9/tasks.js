2235056586c3fabca35982204f33b103a4a55e7b
avganiy <ekramarets@gmail.com>
2022-11-09 14:46:53 +0200

    added hw 9

--- homework9/tasks.js
+++ homework9/tasks.js
@@ -1,1 +1,26 @@
 // task 1
+function addAttr() {
+   $('a[href^="https://"]').attr('target', '_blank');
+}
+
+// taks 2
+function editClass() {
+   $('h2.head').css('background-color', 'green');
+   $('h2.head .inner').css('fontSize', '35px');
+}
+
+// task 3
+function changeOrder() {
+   $('h3 + div').each(function () {
+      const divTagEl = $(this);
+      divTagEl.prev().before(divTagEl);
+   });
+}
+
+// task 4
+const $cbx = $('[type=checkbox]');
+$cbx.change(() => {
+   if ($('[type=checkbox]:checked').length == 3) {
+      $(':checkbox(:checked)').prop('disabled', true);
+   }
+});
